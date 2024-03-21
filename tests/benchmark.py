ccbd4d1906b5b5751bd8122d6e74c0435a1df3a6
Ivan Karpukhin <karpuhini@yandex.ru>
2024-03-21 14:40:15 +0300

    Improve tests and add speed benchmakr

---
+++ tests/benchmark.py
@@ -0,0 +1,32 @@
+import logging
+import os
+import time
+
+import torch
+from torch_linear_assignment import batch_linear_assignment
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    cost = torch.randn(1024, 20, 40)
+    start = time.time()
+    for _ in range(10):
+        batch_linear_assignment(cost)
+    total = time.time() - start
+    logging.info(f"CPU time: {total} s.")
+    if device == "cuda":
+        cost = cost.to(device)
+        start = time.time()
+        for _ in range(10):
+            batch_linear_assignment(cost)
+        torch.cuda.synchronize()
+        total = time.time() - start
+        logging.info(f"GPU time: {total} s.")
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -6,20 +6,34 @@ from torch_linear_assignment import batch_linear_assignment
 
 
 class TestAssignment(TestCase):
+    def setUp(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
     def test_simple(self):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
         cost = torch.tensor([
             8, 4, 7,
             5, 2, 3,
             9, 6, 7,
             9, 4, 8
-        ]).reshape(1, 4, 3).to(device)
+        ]).reshape(1, 4, 3).to(self.device)
         gt_assignment = torch.tensor(
             [0, 2, -1, 1]
         ).reshape(1, 4)
         result = batch_linear_assignment(cost).cpu()
         print(result)
         self.assertTrue((result == gt_assignment).all())
+
+    def test_cuda_equal_to_cpu(self):
+        if self.device == "cpu":
+            return
+
+        for bs, rows, cols in [(16, 20, 40), (1, 30, 10), (0, 5, 5)]:
+            cost = torch.randn(bs, rows, cols)
+            matching_cpu = batch_linear_assignment(cost)
+            matching_gpu = batch_linear_assignment(cost.to(self.device)).cpu()
+            self.assertEqual(matching_cpu.shape, matching_gpu.shape)
+            self.assertEqual(matching_cpu.dtype, matching_gpu.dtype)
+            self.assertTrue((matching_cpu == matching_gpu).all())
 
 
 if __name__ == "__main__":
